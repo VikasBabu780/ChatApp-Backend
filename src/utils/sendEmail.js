64d054c4007df3ebd6c16c32e73a0672b7ff82df
Vikas Babu <23bma048@nith.ac.in>
2026-08-11 10:21:07 +0530

    email issue fixed

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -4,7 +4,7 @@ export const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
-    secure: true,
+    secure: Number(process.env.SMTP_PORT) === 465,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
